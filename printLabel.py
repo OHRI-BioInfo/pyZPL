939fb6b7dd51c2845ff2d436e964658e6645d230
John Brooks <john@fastquake.com>
2014-08-01 13:52:14 -0400

    Iterate through boxes, create rows, generate ZPL command

--- printLabel.py
+++ printLabel.py
@@ -1,15 +1,53 @@
 #!/usr/bin/env python
 
+DPI = 203 #dots/inch
+width = 4 #inches
+height = 6 #inches
+elementSpacing = 100 #dots
+
 import xml.etree.ElementTree as ET
 import sys
+import re
 from pyZPL import *
+
+dotswide = DPI*width
+dotshigh = DPI*height
+
+remainingWidth = dotswide
+remainingHeight = dotshigh
+
+currentDown = 0
+currentRow = 0
+rowHeight = 0
+currentRowElement = 0
 
 tree = ET.parse("testlabel.xml")
 root = tree.getroot()
 
-boxes = []
+rowBoxes = []
+ZPLLayout = "^CF0,30,30^XA"
 
 customIndex = 1
+
+def newRow():
+    global currentDown 
+    global remainingHeight
+    global remainingWidth
+    global currentRow
+    global rowHeight
+    global ZPLLayout
+
+    currentRight = 0
+    for i in range(0,currentRowElement):
+        ZPLLayout = ZPLLayout.replace("+row"+str(currentRow)+"Down",str(currentDown))
+        ZPLLayout = ZPLLayout.replace("+row"+str(currentRow)+"Right"+str(i),str(currentRight))
+        currentRight += int(rowBoxes[i].width) + elementSpacing
+
+    currentDown += rowHeight + elementSpacing
+    remainingHeight -= rowHeight + elementSpacing
+    remainingWidth = dotswide
+    rowHeight = 0
+    currentRow += 1
 
 for element in root.iter():
     if element.get("custom") is not None:
@@ -25,8 +63,37 @@ for element in root.iter():
                     element.find("..").remove(element)
         customIndex += 1
 
-#for box in root.findall("Box"):
-#    newbox = ZPLBox()
-#    newbox.width = int(box.get("width"))
-#    newbox.height = int(box.get("height"))
-#    boxes.append(newbox)
+for element in root.iter():
+    if element.tag == "Box":
+        boxWidth = element.get("width")
+        boxHeight = element.get("height")
+        border = element.get("border")
+
+        if border is None:
+            border = "10"
+        
+        newbox = ZPLBox()
+        newbox.height = boxHeight
+        newbox.width = boxWidth
+        newbox.border = border
+        rowBoxes.append(newbox)
+
+        if int(boxHeight) > rowHeight:
+            rowHeight = int(boxHeight)
+        if remainingWidth is not dotswide:
+            remainingWidth -= elementSpacing
+            if remainingWidth < int(boxWidth)+int(border)*2:
+                newRow()
+        ZPLLayout += "^FO+row"+str(currentRow)+"Right"+str(currentRowElement)+",+row"+str(currentRow)+"Down"
+        remainingWidth -= int(boxWidth)+int(border)*2
+
+        ZPLLayout += "^GB"+boxWidth+","+boxHeight+","+border+"^FS"
+        currentRowElement += 1
+#        for boxchild in element.iter():
+#            if element.tag == "Text":
+
+newRow()
+
+ZPLLayout += "^XZ"
+
+print ZPLLayout
--- pyZPL.py
+++ pyZPL.py
@@ -1,3 +1,4 @@
 class ZPLBox:
     width = 0
     height = 0
+    border = 0
